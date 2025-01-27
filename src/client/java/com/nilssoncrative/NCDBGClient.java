5625b555570516d5e065efb7e302405547341cb4
NilssonCreative <anders@nerdbynature.se>
2025-01-27 11:01:40 +0100

    Initial commit

---
+++ src/client/java/com/nilssoncrative/NCDBGClient.java
@@ -0,0 +1,10 @@
+package com.nilssoncrative;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NCDBGClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/nilssoncrative/NCDBGDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nilssoncrative;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NCDBGDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
